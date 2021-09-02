b9c04579ad86784c6461b7ccd142075730aebb24
Brian Phan <bbphan82@gmail.com>
2021-09-02 22:17:13 +0100

    modified code for arithetic_arranger challeng

--- arithmetic_arranger.py
+++ arithmetic_arranger.py
@@ -1,4 +1,30 @@
+
+
 def arithmetic_arranger(problems):
 
+    if len(problems) > 5:
+        return f"Error: Too many problems."
+    else:
+        elements = " ".join([str(x) for x in problems])
+        print(elements)
+        if elements in "/" "*":
+            return f"Error: Operator must be '+' or '-'"
+        else:
+            arranged_problems = problems
+        #print(type(arranged_problems))
+
+        for i in problems:
+            #num1 = problems[0]
+            #num2 = i[2]
+            #operand = i[1]
+            #print(i)
+        #num1 = i[0]
+        #print(num1)
+    
 
     return arranged_problems
+
+
+#arithmetic_arranger()
+
+#["32 + 698", "3801 - 2", "45 + 43", "123 + 49"]
